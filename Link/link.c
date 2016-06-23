1bd6174df4a75caf157f23c0d4827369fa62efe6
joeyoungisme <joeyoungisme@gmail.com>
2016-06-23 17:47:43 +0000

    link : add general function Init, Add, List, Close

--- Link/link.c
+++ Link/link.c
@@ -1,12 +1,60 @@
 #include "link.h"
 
+LinkList *LinkList_FindEnd(LinkList *head)
+{
+	if(head == NULL) return NULL;
+
+	if(head -> pNext == NULL) return head;
+	
+	return LinkList_FindEnd(head -> pNext);
+}
+
+LinkList *LinkList_NewItem()
+{
+	return malloc(sizeof(struct _LinkList));
+}
+
+void LinkList_Print(LinkList *head)
+{
+	if(head == NULL) return;
+	else
+	{
+		printf(" -> %4d", head -> data);
+		LinkList_Print(head -> pNext);
+	}
+}
+
+void LinkList_Free(LinkList *head)
+{
+	if(head -> pNext == NULL) return;
+	else
+	{
+		LinkList_Free(head -> pNext);
+		free(head -> pNext);
+	}
+}
+
 int LinkList_Init()
 {
-	//init...
+	root = NULL;
 }
 
 int LinkList_Add(int data)
 {
+	LinkList *pEnd = NULL;
+
+	if (root == NULL)
+	{
+		root = LinkList_NewItem();
+		root -> data = data;
+	}
+	else
+	{
+		pEnd = LinkList_FindEnd(root);
+		pEnd -> pNext = LinkList_NewItem();
+		pEnd -> pNext -> data = data;
+	}
+
 	return 0;
 }
 
@@ -18,7 +66,13 @@ int LinkList_Get()
 
 int LinkList_List()
 {
-	printf("Head : \n");
+	printf("Root");
+	LinkList_Print(root);
+	printf("\n");
 	return 0;
 }
 
+void LinkList_Close()
+{
+	LinkList_Free(root);
+}
--- Link/link.h
+++ Link/link.h
@@ -9,6 +9,13 @@ int LinkList_Init();
 int LinkList_Add(int data);
 int LinkList_Get();
 int LinkList_List();
+void LinkList_Close();
 
+typedef struct _LinkList{
+	struct _LinkList *pNext;
+	int data;
+}LinkList;
+
+static LinkList *root;
 
 #endif
